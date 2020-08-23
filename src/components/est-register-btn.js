dd1ea01e14a7e094d8d196c3698e04eeb59d8a6a
Stephanie <almeidasmail@gmail.com>
2020-08-23 14:57:04 -0400

    Add link to register button

--- src/components/est-register-btn.js
+++ src/components/est-register-btn.js
@@ -1,7 +1,9 @@
 const template = document.createElement('template');
 template.innerHTML = `
+<a href="https://registration.teeny.hack.gt" target="_black" rel="noopener">
 <button class="est-register-btn"> 
 </button>
+</a>
 `;
 
 /**
