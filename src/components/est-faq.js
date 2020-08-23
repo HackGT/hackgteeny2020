a970168a22098158b38f4a2282f97decd6789baa
Stephanie <almeidasmail@gmail.com>
2020-08-23 10:50:44 -0400

    Update with new faq assets

--- src/components/est-faq.js
+++ src/components/est-faq.js
@@ -48,18 +48,34 @@ class EstFaq extends HTMLElement {
       const faqContainer = document.createElement('div');
       faqContainer.classList.add('est-faq__container');
 
+      const q = document.createElement('div');
+      q.classList.add('est-faq__container__item--q');
+
       const question = document.createElement('div');
       question.innerHTML = faq.question;
       question.classList.add('est-faq__container__item');
       question.classList.add('est-faq__container__item--question');
+
+      const questionContainer = document.createElement('div');
+      questionContainer.classList.add('est-faq__container__item--question-container');
+      questionContainer.appendChild(q);
+      questionContainer.appendChild(question);
+
+      const a = document.createElement('div');
+      a.classList.add('est-faq__container__item--a');
 
       const answer = document.createElement('div');
       answer.innerHTML = faq.answer;
       answer.classList.add('est-faq__container__item');
       answer.classList.add('est-faq__container__item--answer');
 
-      faqContainer.appendChild(question);
-      faqContainer.appendChild(answer);
+      const answerContainer = document.createElement('div');
+      answerContainer.classList.add('est-faq__container__item--answer-container');
+      answerContainer.appendChild(answer);
+      answerContainer.appendChild(a);
+
+      faqContainer.appendChild(questionContainer);
+      faqContainer.appendChild(answerContainer);
 
       container.appendChild(faqContainer);
     });
